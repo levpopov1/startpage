d59a78407b9c1ff88b1d4e0d9c206d1be49a56d0
Lev Popov <levpopov1@gmail.com>
2023-03-05 17:38:20 +0000

    use queryClient and devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import NotFound from './views/NotFound';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
