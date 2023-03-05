81f29821b9a562ef5f6ab268658555635c76c1ac
Lev Popov <levpopov1@gmail.com>
2023-03-05 16:14:37 +0000

    set up vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </div>
   );
 }
