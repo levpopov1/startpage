daccc39303763e1b693083d88141471475c0288f
Lev Popov <levpopov1@gmail.com>
2023-03-13 13:20:49 +0000

    add settings page placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import NotFound from './views/NotFound';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />}>
         <Route index element={<Home />} />
+        <Route path="settings" element={<Settings />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,9 @@
+function Settings() {
+  return (
+    <div className="settings">
+      <h1 className="text-3xl font-bold">Settings page</h1>
+    </div>
+  );
+}
+
+export default Settings;
