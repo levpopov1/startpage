bed61f571e9ae0345bcba1667f634d8713c42786
Lev Popov <levpopov1@gmail.com>
2023-04-13 15:03:42 +0100

    fix styling classes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,12 +14,12 @@ type Props = {
 };
 
 function Card({ location, size, children }: Props) {
-  // const rowStart = `row-start-${location.row}`;
-  // const rowSpan = `row-span-${size.height}`;
-  // const colStart = `col-start-${location.column}`;
-  // const colSpan = `col-span-${size.width}`;
+  const rowStart = `row-start-${location.row}`;
+  const rowSpan = `row-span-${size.height}`;
+  const colStart = `col-start-${location.column}`;
+  const colSpan = `col-span-${size.width}`;
+  const position = `${rowStart} ${rowSpan} ${colStart} ${colSpan}`;
 
-  const position = `row-start-${location.row} row-span-${size.height} col-start-${location.column} col-span-${size.width}`;
   return <div className={position}>{children}</div>;
 }
 
